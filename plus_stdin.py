2b41903177ab6d2f3b5abbc4a332c940268431cf
Kohta Yoshiba <f.leaves.y.28@gmail.com>
2023-10-19 11:37:07 +0900

    Support integer only calculat ion

--- plus_stdin.py
+++ plus_stdin.py
@@ -1,8 +1,10 @@
- #!/usr/bin/python3
-  import sys
+#!/usr/bin/python3
+import sys
 
-  ans = 0.0
-  for line in sys.stdin:
-      ans += float(line)
-
-  print(ans)
+ans = 0
+for line in sys.stdin:
+    try:
+        ans += int(line)
+    except:
+        ans += float(line)
+print(ans)
