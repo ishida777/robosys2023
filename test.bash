316f58eba0e900c5a0368293f61553ef53cad509
Kohta Yoshiba <f.leaves.y.28@gmail.com>
2023-11-27 11:23:43 +0900

    practice

--- test.bash
+++ test.bash
@@ -1,5 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-Locense-Identifier: BSD-3-Clause
 
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 14 ] || ng ${LINED}
 
-["$[out]" = 15]
+[ "$res" = 0] && echo OK
+exit $res
