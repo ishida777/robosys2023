aad6efaed8ef1d634481d0bfaddf7e355ed14610
Kohta Yoshiba <f.leaves.y.28@gmail.com>
2023-11-27 16:02:41 +0900

    not fix

--- test.bash
+++ test.bash
@@ -1,26 +1,17 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
-# SPDX-Licenese-Identifier: BSD-3-Clause
-
+# SPDX-License-Identifier: BSD-3-Clause
+　
 ng () {
-	echo NG at Line $1
-	res=1
+      echo NG at Line $1
+      res=1
 }
-
+　
 res=0
-+ res=0
-
+　
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
-[ "${out}" = 15 ] || ng ${LINENO}
-+ '[' 15 = 15 ']'
-
-[ "$res" = 0 ]&& echo OK
-+'['0 = 0']'
-+ echo OK
-OK
+[ "${out}" = 14 ] || ng ${LINENO}
+　
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
 exit $res
-+ exit 0
