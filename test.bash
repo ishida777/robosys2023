ec50798602b6ee510a816c33d5949b764df71853
Kohta Yoshiba <f.leaves.y.28@gmail.com>
2023-11-27 16:21:11 +0900

    perfect

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Kota Yoshiba
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,15 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINED}
+
+### STRAGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINED}
+[ "${out}" = "" ] || ng ${LINED}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINED}
+[ "${out}" = "" ] || ng ${LINED}
 
 [ "$res" = 0 ] && echo OK
 exit $res
